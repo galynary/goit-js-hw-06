1dc7702dbeda57fcf927a177078704641c585c2d
galynary <107271972+galynary@users.noreply.github.com>
2022-11-07 10:16:23 +0200

    edit function

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,7 @@
 function getRandomColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const changeButton = document.querySelector("button.change-color");
